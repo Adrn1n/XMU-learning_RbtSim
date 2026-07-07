f8e3df81a65919018e4d90d5f3be6411f6ddd19e
Exxon <exxonliu001@yahoo.com>
2026-07-08 01:26:37 +0800

    Init: Lab1

---
+++ Code/Lab1/run.sh
@@ -0,0 +1,27 @@
+rcssclient -id 1 -sidel &
+sleep 1
+rcssclient -id 2 -sidel &
+sleep 1
+rcssclient -id 3 -sidel &
+sleep 1
+rcssclient -id 4 -sidel &
+sleep 1
+rcssclient -id 5 -sidel &
+sleep 1
+rcssclient -id 6 -sidel &
+sleep 1
+
+rcssclient -id 1 -sider &
+sleep 1
+rcssclient -id 2 -sider &
+sleep 1
+rcssclient -id 3 -sider &
+sleep 1
+rcssclient -id 4 -sider &
+sleep 1
+rcssclient -id 5 -sider &
+sleep 1
+rcssclient -id 6 -sider &
+sleep 1
+
+
